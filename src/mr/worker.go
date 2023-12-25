7a173053ff424817842666307fa08b718071faf3
Andrew Liu <109386884+aliu10523@users.noreply.github.com>
2023-12-25 15:31:28 -0500

    worker implementation draft

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -5,44 +5,48 @@ import "log"
 import "net/rpc"
 import "hash/fnv"
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
+	shouldExit := false
+	for !shouldExit {
+		args := RequestTaskArgs{}
+		reply := RequestTaskReply{}
+		call("Coordinator.RequestTask", &args, &reply)
+		switch reply.TaskType {
+		case MapTask:
+			doMapTask(reply.TaskID, reply.InputFiles, reply.NReduce, mapf)
+		case ReduceTask:
+			doReduceTask(reply.TaskID, reply.InputFiles, reducef)
+		case Exit:
+			shouldExit = true
+		}
 
+	}
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
 
 }
 
-//
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -61,11 +65,9 @@ func CallExample() {
 	fmt.Printf("reply.Y %v\n", reply.Y)
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
